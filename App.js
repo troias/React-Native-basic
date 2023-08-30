218b070294233533e8a92813f3dd37bba407b6e3
troias <hunter007nz@gmail.com>
2023-08-30 23:27:54 +1200

    added layout

--- App.js
+++ App.js
@@ -7,29 +7,10 @@ export default function App() {
 
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-            width: "80%",
-            borderRadius: 10,
-            width: "60%",
-          }}
-          placeholder="Your current goal!"
-        />
-        <Button
-          styles={{
-            width: "40%",
-            borderRadius: 10,
-            paddingRight: 10,
-            paddingLeft: 10,
-            borderRadius: 10,
-          }}
-          title="Add Goal"
-        />
+        <TextInput style={styles.TextInput} placeholder="Your current goal!" />
+        <Button style={styles.button} title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text style={styles.textHeading}>List of Goals</Text>
         <Text>Goal 1</Text>
         <Text>Goal 2</Text>
@@ -41,18 +22,47 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 20,
+    padding: 16,
     paddingTop: 50,
+    flexDirection: "column",
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    borderBottomColor: "gray",
+    borderBottomWidth: 1,
+    marginBottom: 10,
+  },
+  goalsContainer: {
+    flex: 6,
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    marginTop: 10,
+    borderRadius: 10,
   },
   textHeading: {
     fontSize: 30,
     fontWeight: "bold",
     paddingTop: 20,
     paddingBottom: 20,
+  },
+  TextInput: {
+    width: "70%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+  button: {
+    width: "40%",
+    borderRadius: 10,
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderRadius: 10,
   },
 });
